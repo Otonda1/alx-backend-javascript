1ddf49155db5ca8ac032b293cc9c23b6d40aad75
Charles Otonda <charlesotonda@gmail.com>
2024-01-05 21:40:28 +0000

    class method that calls a constructor

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,26 +1,17 @@
 export default class Building {
   constructor(sqft) {
-    if (this.constructor !== Building) {
-      if (typeof this.evacuationWarningMessage !== "function") {
-        throw new Error(
-          "Class extending Building must override evacuationWarningMessage"
-        );
-      }
-    }
-    if (typeof sqft !== "number") {
-      throw new Error("sqft must be a number");
+    if (
+      new.target !== Building
+      && typeof this.evacuationWarningMessage !== 'function'
+    ) {
+      throw new Error(
+        'Class extending Building must override evacuationWarningMessage',
+      );
     }
     this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
-  }
-
-  set sqft(size) {
-    if (typeof size !== "number") {
-      throw new Error("Type of size must be number");
-    }
-    this._sqft = size;
   }
 }
