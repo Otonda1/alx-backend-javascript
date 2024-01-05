08d3fe46c36b4d5aaae7dcc79b05ede1a39e7aee
Charles Otonda <charlesotonda@gmail.com>
2024-01-05 21:05:30 +0000

    remailning tasks

---
+++ 0x01-ES6_promise/8-try.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+export default async function divideFunction(numerator, denominator) {
+    try {
+        if (denominator === 0) {
+            throw new Error('cannot divide by 0');
+        } else {
+            return numerator / denominator;
+        }
+    } catch (error) {
+        return error;
+    }
+}
