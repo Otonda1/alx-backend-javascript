70385668289f8dbc820be83b9199c48dc276dee5
Charles Otonda <charlesotonda@gmail.com>
2024-01-09 17:54:31 +0000

    a function thta returns a list of student objects

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,9 +1,9 @@
 // a function that returns a list of studen objects
 export default function getListStudents() {
   return [
-	  {id: 1, firstName: 'Guillaume', location: 'San Francisco'},
-	  {id: 2, firstName: 'James', location: 'Columbia'},
-	  {id: 5, firstName: 'Serena', location: 'San Francisco'}
+    {id: 1, firstName: 'Guillaume', location: 'San Francisco'},
+    {id: 2, firstName: 'James', location: 'Columbia'},
+    {id: 5, firstName: 'Serena', location: 'San Francisco'}
   ];
 
 }
